701beb0b514c92959d83394217e29b0bf34dd351
Gabriel Bomtempo <gabrielbomlima@gmail.com>
2022-08-24 15:12:46 -0300

    create-contact-page

--- show.php
+++ show.php
@@ -4,13 +4,14 @@
 
 ?>
 
-   <div class="container" id="view-contact-container">
-    <h1 id="main-title"><?= $contact["name"] ?></h1>
-    <p class="bold">Telefone:</p>
-    <p><?= $contact["phone"] ?></p>
-    <p class="bold">Observações:</p>
-    <p><?= $contact["observations"] ?></p>
-   </div>
+    <div class="container" id="view-contact-container">
+        <?php include_once("templates/backbtn.html"); ?>
+        <h1 id="main-title"><?= $contact["name"] ?></h1>
+        <p class="bold">Telefone:</p>
+        <p><?= $contact["phone"] ?></p>
+        <p class="bold">Observações:</p>
+        <p><?= $contact["observations"] ?></p>
+    </div>
 
 <?php
 
