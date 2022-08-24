dceef27d0f250a439de786a69562466a707b8363
Gabriel Bomtempo <gabrielbomlima@gmail.com>
2022-08-24 18:47:08 -0300

    contact-deleting-done

--- config/process.php
+++ config/process.php
@@ -73,6 +73,31 @@
             }
 
         }
+        else if($data["type"] === "delete") {
+
+            $id = $data["id"];
+
+            $query = "DELETE FROM contacts WHERE id = :id";
+
+            $stmt = $conn->prepare($query);
+
+            $stmt->bindParam(":id", $id);
+
+            try {
+
+                $stmt->execute();
+                $_SESSION["msg"] = "Contato removido com sucesso!";
+
+            }
+            catch(PDOException $e) {
+
+                // Connection error
+                $error = $e->getMessage();
+                echo "Erro: $error";
+
+            }
+
+        }
 
         // REDIRECT TO HOME
         header("Location:" . $BASE_URL . "../index.php");
